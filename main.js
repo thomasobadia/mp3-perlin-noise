889c55484cfda0bccc0cc3a4f414cc723b95fbd8
Thomas Obadia <thomas.obadia@hetic.net>
2019-07-25 14:45:56 +0200

    mp3 animation

--- main.js
+++ main.js
@@ -4,43 +4,49 @@ let container = document.getElementById('container')
 let el = document.getElementById('audio')
 
 
-const perlinMovement = () => {
+const perlinMovement = (analyser,data) => {
 
-        setInterval(() => {
-            while (container.firstChild) {
-                container.removeChild(container.firstChild);
-            }
-            for (let y = 0; y < 1; y += 0.01) {
-                let bloc = document.createElement("div"); 
-                bloc.className = 'test-box'
-                bloc.style.height = (1 - noise.perlin2(t, y*5)) * volume +'px'
-                bloc.style.left =  y * 50 +'%'
-                container.appendChild(bloc); 
+    let linesArray = createLines() 
+    setInterval(() => {
+        // while (container.firstChild) {
+        //     container.removeChild(container.firstChild);
+        // }
+        console.log(linesArray)
+        let volume = getVolume(analyser,data)
 
-            }
-            t +=0.005
-        } , 10
+        for(let y = 0; y <linesArray.length ; y++){
+            let range = (1 - noise.perlin2(t, y*0.05)) * volume
+            let height = scale(range, 0, 60, 10, 20 )
+            linesArray[y].setAttribute('x2', height)
+        }
 
-        )
-        
+        t +=0.001
+    } , 10
 
+    )
+    
+
+}
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
 
 
-
-
 el.addEventListener('play', () => {
-    playSound()
+    initSound ()
 })
 
 
- function playSound() {
+
+
+function initSound (){
+
+
     var ctx = new AudioContext();
     var audioSrc = ctx.createMediaElementSource(el);
     var analyser = ctx.createAnalyser();
-
-
     analyser.smoothingTimeConstant = 0.3;
     analyser.fftSize = 1024;
 
@@ -48,20 +54,38 @@ el.addEventListener('play', () => {
     audioSrc.connect(ctx.destination);
    
     var frequencyData = new Uint8Array(analyser.frequencyBinCount);
+
+
+    perlinMovement(analyser,frequencyData)
    
-    function renderFrame() {
-       requestAnimationFrame(renderFrame);
-       // update data in frequencyData
-       analyser.getByteFrequencyData(frequencyData);
-       volume = Math.floor(Math.average(frequencyData));
+}
+
+function createLines () {
+
+    let lines = []
+    for (let y = 0; y < 1; y += 0.01) {
+        let line = document.createElementNS('http://www.w3.org/2000/svg','line');
+        line.setAttribute('transform',"rotate(" + y * 360 + ")")
+        line.setAttribute('x1','10')
+        line.setAttribute('y1','0')
+        line.setAttribute('y2','0')
+        line.setAttribute('x2', 16)
+        container.appendChild(line); 
+        lines.push(line)
     }
+    return lines
+
+}
+
+function getVolume(analyser, data){
+    analyser.getByteFrequencyData(data);
+    volume = Math.floor(Math.average(data));
+
+    return volume
+
+}
 
 
-    renderFrame();
-    perlinMovement()
-
-
-  };
 
 
 
