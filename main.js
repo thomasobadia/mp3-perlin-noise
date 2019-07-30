edb73faa14181ef61a3465d00875a7345733fc09
Thomas Obadia <thomas.obadia@hetic.net>
2019-07-30 16:26:09 +0200

    safari compat

--- main.js
+++ main.js
@@ -7,26 +7,26 @@ let audioText = document.querySelector('.audio-text')
 let init = true
 
 
-const perlinMovement = (analyser,data) => {
+const perlinMovement = (analyser, data) => {
 
-    let linesArray = createLines() 
+    let linesArray = createLines()
     setInterval(() => {
-        let volume = getVolume(analyser,data)
+            let volume = getVolume(analyser, data)
 
-        for(let y = 0; y <linesArray.length ; y++){
-            let range = (1 - noise.perlin2(t, y*0.05)) * volume
-            let height = scale(range, 0, 60, 20, 32 )
-            if(height < 20){
-                height =20
+            for (let y = 0; y < linesArray.length; y++) {
+                let range = (1 - noise.perlin2(t, y * 0.05)) * volume
+                let height = scale(range, 0, 60, 20, 32)
+                if (height < 20) {
+                    height = 20
+                }
+                linesArray[y].setAttribute('x2', height)
             }
-            linesArray[y].setAttribute('x2', height)
-        }
 
-        t +=0.005
-    } , 10
+            t += 0.005
+        }, 10
 
     )
-    
+
 
 }
 
@@ -35,64 +35,76 @@ const scale = (num, in_min, in_max, out_min, out_max) => {
 }
 
 
-button.addEventListener('click',(e)=>{
-    initSound (e.target)
+button.addEventListener('click', (e) => {
+    initSound(e.target)
 
 })
-button.addEventListener('touch',(e)=>{
-    initSound (e.target)
+button.addEventListener('touch', (e) => {
+    initSound(e.target)
 
 })
 
 
 
 
-function initSound (target){
-    if(init){
-        var ctx = new AudioContext();
-        var audioSrc = ctx.createMediaElementSource(el);
-        var analyser = ctx.createAnalyser();
-        analyser.smoothingTimeConstant = 0.3;
-        analyser.fftSize = 1024;
-    
-        audioSrc.connect(analyser);
-        audioSrc.connect(ctx.destination);
-       
-        var frequencyData = new Uint8Array(analyser.frequencyBinCount);
-        perlinMovement(analyser,frequencyData)
-        el.play()
-        audioText.innerHTML = "pause"
-        init = false
+function initSound(target) {
+    if (init) {
 
-    }else {
-        el.paused? el.play() : el.pause();
-        el.paused? audioText.innerHTML = "écouter" : audioText.innerHTML = "pause"
+        var AudioContext = window.AudioContext // Default
+            ||
+            window.webkitAudioContext // Safari and old versions of Chrome
+            ||
+            false;
+
+        if (AudioContext) {
+            var ctx = new AudioContext();
+            var audioSrc = ctx.createMediaElementSource(el);
+            var analyser = ctx.createAnalyser();
+            analyser.smoothingTimeConstant = 0.3;
+            analyser.fftSize = 1024;
+            audioSrc.connect(analyser);
+            audioSrc.connect(ctx.destination);
+
+            var frequencyData = new Uint8Array(analyser.frequencyBinCount);
+            perlinMovement(analyser, frequencyData)
+            el.play()
+            audioText.innerHTML = "pause"
+            init = false
+        } else {
+            // Web Audio API is not supported
+            alert("Sorry, but the Web Audio API is not supported by your browser. Please, consider upgrading to the latest version or downloading Google Chrome or Mozilla Firefox");
+        }
+
+
+    } else {
+        el.paused ? el.play() : el.pause();
+        el.paused ? audioText.innerHTML = "écouter" : audioText.innerHTML = "pause"
 
     }
-   
+
 }
 
 
 
 
-function createLines () {
+function createLines() {
 
     let lines = []
     for (let y = 0; y < 1; y += 0.01) {
-        let line = document.createElementNS('http://www.w3.org/2000/svg','line');
-        line.setAttribute('transform',"rotate(" + y * 360 + ")")
-        line.setAttribute('x1','20')
-        line.setAttribute('y1','0')
-        line.setAttribute('y2','0')
+        let line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+        line.setAttribute('transform', "rotate(" + y * 360 + ")")
+        line.setAttribute('x1', '20')
+        line.setAttribute('y1', '0')
+        line.setAttribute('y2', '0')
         line.setAttribute('x2', 20)
-        container.appendChild(line); 
+        container.appendChild(line);
         lines.push(line)
     }
     return lines
 
 }
 
-function getVolume(analyser, data){
+function getVolume(analyser, data) {
     analyser.getByteFrequencyData(data);
     volume = Math.floor(Math.average(data));
 
@@ -105,21 +117,18 @@ function getVolume(analyser, data){
 
 
 
-Math.average = function(arguments) {
+Math.average = function (arguments) {
     var numbers;
     if (arguments[0] instanceof Array) {
         numbers = arguments[0];
-    }
-    else if (typeof arguments[0] == "number") {
+    } else if (typeof arguments[0] == "number") {
         numbers = arguments;
     }
-    var sum		= 0;
-    var average	= 0;
+    var sum = 0;
+    var average = 0;
     for (var i = 0; i < numbers.length; i++) {
         sum += numbers[i];
     }
     average = sum / numbers.length;
     return average;
 }
-
-
